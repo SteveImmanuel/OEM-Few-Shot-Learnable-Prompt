Fix masking, dataset also returns label and color
masking was swapped between 0 and 1

data.py
@@ -6,7 +6,7 @@ import imgaug.augmenters as iaa
 from utils import get_logger
 from typing import Iterable, Tuple, List
 from PIL import Image
-from itertools import combinations
+from itertools import combinations, permutations
 
 class OEMDataset(torch.utils.data.Dataset):
     def __init__(
@@ -15,10 +15,11 @@ class OEMDataset(torch.utils.data.Dataset):
         mean:Iterable[float]=[0.485, 0.456, 0.406], 
         std:Iterable[float]=[0.229, 0.224, 0.225], 
         resize: Tuple[int, int] = (448, 448),
-        max_classes: int = 11,
+        max_classes: int = 11, # excluding background
         patch_size: Tuple[int, int] = (16, 16),
         mask_ratio: float = 0.75,
         half_mask_ratio: float = 0.2,
+        is_train: bool = True,
     ):
         super().__init__()
         assert osp.exists(osp.join(root, 'images')), f'Path {root}/images does not exist'
@@ -28,6 +29,7 @@ class OEMDataset(torch.utils.data.Dataset):
         self.mean = np.array(mean)
         self.std = np.array(std)
         self.resize = resize
+        self.is_train = is_train
         self.max_classes = max_classes
         self.patch_size = patch_size
         self.mask_ratio = mask_ratio
@@ -39,8 +41,8 @@ class OEMDataset(torch.utils.data.Dataset):
             img_path = osp.join(self.root, 'images', path)
             label_path = osp.join(self.root, 'labels', path)
             if not osp.exists(label_path):
-                self.logger.warn(f'Skipping label path {label_path} as it does not exist')
-
+                # self.logger.warn(f'Skipping label path {label_path} as it does not exist')
+                continue
             self.paths.append((img_path, label_path))
         
         self._generate_pairs()
@@ -48,14 +50,17 @@ class OEMDataset(torch.utils.data.Dataset):
 
     def _generate_pairs(self):
         indices = np.arange(len(self.paths))
-        self.pairs = list(combinations(indices, 2))
+        if self.is_train:
+            self.pairs = list(combinations(indices, 2))
+        else:
+            self.pairs = list(permutations(indices, 2))
 
     def _load_img_lbl(self, img_path, label_path):
         img = Image.open(img_path).convert('RGB')
         label = Image.open(label_path).convert('L')
         if self.resize is not None:
             img = img.resize(self.resize)
-            label = label.resize(self.resize)
+            label = label.resize(self.resize, Image.NEAREST)
         img = np.array(img).astype(np.uint8)
         label = np.array(label).astype(np.uint8)
         return img, label
@@ -72,13 +77,14 @@ class OEMDataset(torch.utils.data.Dataset):
             iaa.GaussianBlur((0, 0.05))
         ])
     
-    def _augment(self, img: List[np.ndarray], label: List[np.ndarray]):
+    def _augment(self, img: List[np.ndarray], label: List[np.ndarray], ori_label: List[np.ndarray]):
         aug_all = self.augment_all.to_deterministic()
         all = np.concatenate([img, label], axis=0)
         res = [aug_all.augment_image(x) for x in all]
+        ori_label = [aug_all.augment_image(x) for x in ori_label]
         img, label = res[:len(img)], res[len(img):]
         img = self.augment_img.augment_images(img)
-        return img, label
+        return img, label, ori_label
 
     def _lbl_random_color(self, label: np.ndarray, color_palette: np.ndarray):
         result = np.zeros((label.shape[0], label.shape[1], 3), dtype=np.uint8)
@@ -86,7 +92,7 @@ class OEMDataset(torch.utils.data.Dataset):
             result[label == (i + 1)] = color_palette[i] # 0 is reserved for background
         return result
 
-    def _to_tensor(self, arr: np.ndarray):
+    def _to_img_tensor(self, arr: np.ndarray):
         arr = arr / 255.0
         arr = arr - self.mean
         arr = arr / self.std
@@ -95,38 +101,44 @@ class OEMDataset(torch.utils.data.Dataset):
         return res
     
     def _generate_mask(self, img_shape: Tuple[int, int]):
+        # 1 means masked, 0 means not masked
         total_patch = (img_shape[0] // self.patch_size[0]) * (img_shape[1] // self.patch_size[1])
-        if np.random.rand() < self.half_mask_ratio:
+        if np.random.rand() < self.half_mask_ratio or not self.is_train:
             mask = torch.zeros(total_patch, dtype=torch.float32)
             mask[total_patch//2:] = 1
         else:
-            total_zeros = int(total_patch * self.mask_ratio)
+            total_ones = int(total_patch * self.mask_ratio)
             shuffle_idx = torch.randperm(total_patch)
-            mask = torch.FloatTensor([0] * total_zeros + [1] * (total_patch - total_zeros))[shuffle_idx]
+            mask = torch.FloatTensor([0] * (total_patch - total_ones) + [1] * total_ones)[shuffle_idx]
+
         return mask
 
     def __getitem__(self, idx):
         pair_idx1, pair_idx2 = self.pairs[idx]
-        img1, label1 = self._load_img_lbl(*self.paths[pair_idx1])
-        img2, label2 = self._load_img_lbl(*self.paths[pair_idx2])
-        if np.random.rand() > 0.5: # swap pair
+        img1, ori_label1 = self._load_img_lbl(*self.paths[pair_idx1])
+        img2, ori_label2 = self._load_img_lbl(*self.paths[pair_idx2])
+        if self.is_train and np.random.rand() > 0.5: # swap pair
             img1, img2 = img2, img1
-            label1, label2 = label2, label1
+            ori_label1, ori_label2 = ori_label2, ori_label1
 
         color_palette = self._generate_color_palette()
-        label1 = self._lbl_random_color(label1, color_palette)
-        label2 = self._lbl_random_color(label2, color_palette)
+        label1 = self._lbl_random_color(ori_label1, color_palette)
+        label2 = self._lbl_random_color(ori_label2, color_palette)
 
-        img, label = self._augment([img1, img2], [label1, label2])
+        img, label, ori_label = self._augment([img1, img2], [label1, label2], [ori_label1, ori_label2])
         img = np.concatenate(img, axis=0)
         label = np.concatenate(label, axis=0)
-        img = self._to_tensor(img)
-        label = self._to_tensor(label)
+        ori_label = np.concatenate(ori_label, axis=0)
+        
+        img = self._to_img_tensor(img)
+        label = self._to_img_tensor(label)
+        ori_label = torch.FloatTensor(ori_label)
         
         valid = torch.ones_like(label)
         mask = self._generate_mask((img.shape[1], img.shape[2]))
         seg_type = torch.zeros([1])
-        return img, label, mask, valid, seg_type
+        color_palette = torch.FloatTensor(color_palette)
+        return img, label, mask, valid, seg_type, ori_label, color_palette
 
     def __len__(self):
         return len(self.pairs)
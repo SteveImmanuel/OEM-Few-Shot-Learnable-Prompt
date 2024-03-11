79ac729ce1dfa93bdb7dc6845098ca4c382aa322
SteveImmanuel <steve@telepix.net>
2024-03-11 17:17:14 +0900

    Support batch calculation

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import logging
-import numpy as np
 import torch
-from PIL import Image
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
@@ -13,75 +11,49 @@ def get_logger(name: str, rank: int):
             def no_op(*args, **kwargs):
                 """Accept every signature by doing non-operation."""
                 pass
-
             return no_op
 
     if rank == 0:
         return logging.getLogger(name)
     return NoOp()
 
-def cmap_to_lbl(cmap: torch.tensor, color_palette: torch.tensor):
-    H, W, C = cmap.shape
-    N, _ = color_palette.shape
-    dist_mat = torch.cdist(cmap.reshape(1, H * W, C), color_palette.unsqueeze(0), 2)
-    dist_mat = dist_mat.reshape(H, W, N)
-    label = torch.argmin(dist_mat, 2)
+def cmap_to_lbl(cmap: torch.Tensor, color_palette: torch.Tensor):
+    B, H, W, C = cmap.shape
+    black = torch.zeros(3, dtype=color_palette.dtype, device=color_palette.device).reshape(1, 1, -1).expand(B, -1, -1)
+    ex_color_palette = torch.concatenate([black, color_palette], axis=1)
+    B, N, _ = ex_color_palette.shape
+    
+    dist_mat = torch.cdist(cmap.reshape(B, H * W, C), ex_color_palette, p=2)
+    dist_mat = dist_mat.reshape(B, H, W, N)
+    label = torch.argmin(dist_mat, axis=3)
+
     result = torch.zeros_like(cmap)
-    for i in range(N):
-        result[label == i] = color_palette[i]
+    for i in range(B):
+        for j in range(N):
+            result[i][label[i] == j] = ex_color_palette[i][j]
+
     return result, label
 
-def calculate_iou(pred: torch.tensor, gt: torch.tensor, color_palette: torch.tensor):
-    N, _ = color_palette.shape
-    x = 0
-    for i in range(N):
-        pred_total = (pred == i)
-        gt_total = (gt == i)
+def calculate_iou(pred: torch.Tensor, gt: torch.Tensor, mask: torch.Tensor, total_classes: int):
+    result = torch.zeros((total_classes, 2), dtype=pred.dtype, device=pred.device)
+    masked_gt = mask * gt
+    masked_pred = mask * pred
+    for i in range(1, total_classes + 1):
+        pred_total = (masked_pred == i)
+        gt_total = (masked_gt == i)
         intersection = (pred_total & gt_total).sum()
         union = pred_total.sum() + gt_total.sum() - intersection
-        x += pred_total.sum()
-        print(i, intersection, union, intersection/union)
-    print(x)
+        # print(i, pred_total.sum(), gt_total.sum(), intersection, union)
+        result[i - 1][0] += intersection
+        result[i - 1][1] += union
+    return result
 
 if __name__ == '__main__':
-    color_palette = [
-        [0,0,0],
-        [17, 141,215],
-        [127,173,123],
-        [225, 227,155],
-        [185,122,87],
-        [228,200,182],
-        [150,150,150]
-    ]
-    color_palette = torch.FloatTensor(color_palette)
-
-    img = np.array(Image.open('temp3.png'))
-    img = torch.FloatTensor(img)
-    dcmap, label = cmap_to_lbl(img, color_palette)
-    dcmap = dcmap.numpy().astype(np.uint8)
-    label = label.numpy().astype(np.uint8)
-    label = Image.fromarray(label)
-    label.save('pred.png')
-
-    img = np.array(Image.open('temp2.png'))
-    img = torch.FloatTensor(img)
-    dcmap, label = cmap_to_lbl(img, color_palette)
-    dcmap = dcmap.numpy().astype(np.uint8)
-    label = label.numpy().astype(np.uint8)
-    label = Image.fromarray(label)
-    label.save('gt.png') #make sure to save as PNG for lossless compression
-
-
-    img1 = np.array(Image.open('pred.png'))
-    img1 = torch.FloatTensor(img1)
-    img2 = np.array(Image.open('gt.png'))
-    img2 = torch.FloatTensor(img2)
-    
-    calculate_iou(img1, img2, color_palette)
-    # dcmap, label = cmap_to_lbl(img, color_palette)
-    # dcmap = dcmap.numpy().astype(np.uint8)
-    # label = label.numpy().astype(np.uint8)
-    # dcmap = Image.fromarray(dcmap)
-    # # dcmap.save('dcmap.jpg')
-    # label = Image.fromarray(label)
-    # label.save('gt.jpg')
+    pred = torch.ones((5, 10, 10))
+    gt = torch.ones((5, 10, 10))
+    mask = torch.ones((5, 10, 10))
+    res = calculate_iou(pred, gt, mask, 4)
+    # cmap = torch.randn(10, 500, 400, 3)
+    # cp = torch.randn(10, 20, 3)
+    # cmap_to_lbl(cmap, cp)
+    print(res)

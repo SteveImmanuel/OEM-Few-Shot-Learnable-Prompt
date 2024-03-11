17cebfa218f8b25aa625db92a3169bac8f931ca8
SteveImmanuel <steve@telepix.net>
2024-03-11 18:01:56 +0900

    Group pairs that contains same classes

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ from utils import get_logger
 from typing import Iterable, Tuple, List
 from PIL import Image
 from itertools import combinations, permutations
+from tqdm import tqdm
 
 class OEMDataset(torch.utils.data.Dataset):
     def __init__(
@@ -18,7 +19,6 @@ class OEMDataset(torch.utils.data.Dataset):
         max_classes: int = 11, # excluding background
         patch_size: Tuple[int, int] = (16, 16),
         mask_ratio: float = 0.75,
-        half_mask_ratio: float = 0.2,
         is_train: bool = True,
     ):
         super().__init__()
@@ -33,7 +33,6 @@ class OEMDataset(torch.utils.data.Dataset):
         self.max_classes = max_classes
         self.patch_size = patch_size
         self.mask_ratio = mask_ratio
-        self.half_mask_ratio = half_mask_ratio
         self.logger = get_logger(__class__.__name__, 0) # TODO: Bug, every process will log
         
         self.paths = []
@@ -45,8 +44,21 @@ class OEMDataset(torch.utils.data.Dataset):
                 continue
             self.paths.append((img_path, label_path))
         
+        self._preload_dataset()
         self._generate_pairs()
         self._init_augmentation()
+        self._filter_pairs()
+
+    def _preload_dataset(self):
+        self.images = []
+        self.labels = []
+        self.unique_classes = []
+        for img_path, label_path in tqdm(self.paths, desc='Caching images and labels'):
+            img = self._load_img(img_path)
+            label = self._load_lbl(label_path)
+            self.images.append(img)
+            self.labels.append(label)
+            self.unique_classes.append(set(np.unique(label)))
 
     def _generate_pairs(self):
         indices = np.arange(len(self.paths))
@@ -54,6 +66,31 @@ class OEMDataset(torch.utils.data.Dataset):
             self.pairs = list(combinations(indices, 2))
         else:
             self.pairs = list(permutations(indices, 2))
+    
+    def _filter_pairs(self):
+        self.same_class_pairs = []
+        self.diff_class_pairs = []
+        for pair in tqdm(self.pairs, desc='Filtering pairs'):
+            len_intersect = self.unique_classes[pair[0]].intersection(self.unique_classes[pair[1]])
+            len_union = self.unique_classes[pair[0]].union(self.unique_classes[pair[1]])
+            if len_intersect == len_union:
+                self.same_class_pairs.append(pair)
+            else:
+                self.diff_class_pairs.append(pair)
+
+    def _load_img(self, path):
+        img = Image.open(path).convert('RGB')
+        if self.resize is not None:
+            img = img.resize(self.resize)
+        img = np.array(img).astype(np.uint8)
+        return img
+    
+    def _load_lbl(self, path):
+        label = Image.open(path).convert('L')
+        if self.resize is not None:
+            label = label.resize(self.resize, Image.NEAREST)
+        label = np.array(label).astype(np.uint8)
+        return label
 
     def _load_img_lbl(self, img_path, label_path):
         img = Image.open(img_path).convert('RGB')
@@ -100,10 +137,10 @@ class OEMDataset(torch.utils.data.Dataset):
         res = torch.einsum('hwc->chw', res)
         return res
     
-    def _generate_mask(self, img_shape: Tuple[int, int]):
+    def _generate_mask(self, img_shape: Tuple[int, int], is_half: bool = False):
         # 1 means masked, 0 means not masked
         total_patch = (img_shape[0] // self.patch_size[0]) * (img_shape[1] // self.patch_size[1])
-        if np.random.rand() < self.half_mask_ratio or not self.is_train:
+        if is_half:
             mask = torch.zeros(total_patch, dtype=torch.float32)
             mask[total_patch//2:] = 1
         else:
@@ -114,12 +151,19 @@ class OEMDataset(torch.utils.data.Dataset):
         return mask
 
     def __getitem__(self, idx):
-        pair_idx1, pair_idx2 = self.pairs[idx]
-        img1, ori_label1 = self._load_img_lbl(*self.paths[pair_idx1])
-        img2, ori_label2 = self._load_img_lbl(*self.paths[pair_idx2])
-        if self.is_train and np.random.rand() > 0.5: # swap pair
-            img1, img2 = img2, img1
-            ori_label1, ori_label2 = ori_label2, ori_label1
+        if self.is_train:
+            if idx < len(self.same_class_pairs):
+                pair_idx1, pair_idx2 = self.same_class_pairs[idx]
+            else:
+                pair_idx1, pair_idx2 = self.diff_class_pairs[idx - len(self.same_class_pairs)]
+
+            if np.random.rand() > 0.5: # swap pair
+                pair_idx1, pair_idx2 = pair_idx2, pair_idx1
+        else:
+            pair_idx1, pair_idx2 = self.same_class_pairs[idx]
+
+        img1, ori_label1 = self.images[pair_idx1], self.labels[pair_idx1]
+        img2, ori_label2 = self.images[pair_idx2], self.labels[pair_idx2]
 
         color_palette = self._generate_color_palette()
         label1 = self._lbl_random_color(ori_label1, color_palette)
@@ -134,22 +178,31 @@ class OEMDataset(torch.utils.data.Dataset):
         label = self._to_img_tensor(label)
         ori_label = torch.FloatTensor(ori_label)
         
+        if not self.is_train:
+            is_half = True
+        else:
+            is_half = idx < len(self.same_class_pairs)
+        mask = self._generate_mask((img.shape[1], img.shape[2]), is_half)
         valid = torch.ones_like(label)
-        mask = self._generate_mask((img.shape[1], img.shape[2]))
         seg_type = torch.zeros([1])
         color_palette = torch.FloatTensor(color_palette)
         return img, label, mask, valid, seg_type, ori_label, color_palette
 
     def __len__(self):
-        return len(self.pairs)
+        if self.is_train:
+            return len(self.same_class_pairs) + len(self.diff_class_pairs)
+        return len(self.same_class_pairs)
 
 if __name__ == '__main__':
-    dataset = OEMDataset('/home/steve/Datasets/OpenEarthMap-FSS/trainset', half_mask_ratio=0)
+    dataset = OEMDataset('/home/steve/Datasets/OpenEarthMap-FSS/trainset', is_train=True)
+    for i in tqdm(range(len(dataset))):
+        a = dataset[i]
     # img, label = dataset[0]
-    img, label, mask, valid, color_palette = dataset[0]
-    img = img.cpu().numpy()
-    label = label.cpu().numpy()
-    img = (((img * dataset.std) + dataset.mean) * 255).astype(np.uint8)
-    lbl = (((label * dataset.std) + dataset.mean) * 255).astype(np.uint8)
-    print(img.shape, label.shape)
-    print(mask)
+    # img, label, mask, valid, seg_type, ori_label, color_palette = dataset[0]
+    # for i in range(len(dataset))
+    # img = img.cpu().numpy()
+    # label = label.cpu().numpy()
+    # img = (((img * dataset.std) + dataset.mean) * 255).astype(np.uint8)
+    # lbl = (((label * dataset.std) + dataset.mean) * 255).astype(np.uint8)
+    # print(img.shape, label.shape)
+    # print(mask)

5638f4bccf97f1ce83583100211abcb6ac412d8a
SteveImmanuel <steve@telepix.net>
2024-03-19 14:36:28 +0900

    Update agent

--- agent.py
+++ agent.py
@@ -288,187 +288,6 @@ class Agent():
         deque(self.process_data(test_dataloader, False, 0), maxlen=0)
         self.logger.info(f'Loss: {self.last_loss:.5f}')
 
-class AgentAdapter(Agent):
-    def step(
-        self,
-        img: T.Tensor,
-        label: T.Tensor,
-        mask: T.Tensor,
-        valid: T.Tensor,
-        seg_type: T.Tensor,
-        is_train: bool,
-    ):
-        img = img.to(self.gpu_id)
-        label = label.to(self.gpu_id)
-        mask = mask.to(self.gpu_id)
-        valid = valid.to(self.gpu_id)
-        seg_type = seg_type.to(self.gpu_id)
-
-        with T.cuda.amp.autocast():
-            feature_ensemble = -1 # TODO: test change to 0 to enable
-            loss, pred, bool_masked_pos = self.model(img, label, mask, valid, seg_type, feature_ensemble)
-
-        if is_train:
-            self.optim.zero_grad()
-            self.scaler.scale(loss).backward()
-            self.scaler.step(self.optim)
-            self.scaler.update()
-        # print(pred.shape, pred[0].shape)
-        # print(tgts.shape, tgts[0].shape)
-        # a = self.model.module.seggpt.unpatchify(pred)
-        # a = a[0].permute(1,2,0)
-        # b = tgts[0].permute(1,2,0)
-        # print(a.shape, b.shape)
-        # a = a[a.shape[0]//2:, :, :]
-        # b = b[b.shape[0]//2:, :, :]
-        # print(a.shape, b.shape)
-        # c = T.cat([a, b], dim=1)
-        # print('loss before unnormalized', F.smooth_l1_loss(a, b, beta=0.01))
-        # self.summary_writer.add_image('testing view', c, 0, dataformats='HWC')
-        return loss.item(), pred, bool_masked_pos
-    
-    def visualize(self, title:str, imgs: T.Tensor, labels: T.Tensor, preds: T.Tensor, discretized_cmaps: T.Tensor, masks: T.Tensor, counter:int = None):
-        if self.log_enabled and self.gpu_id == 0 and (self.counter % self.args['image_log_interval'] == 0 or counter is not None):
-            counter = counter if counter is not None else self.counter
-            all_res = []
-            for i in range(len(preds)):
-                input_img = imgs[i].cpu()
-                label = labels[i].cpu()
-                pred = preds[i].cpu()
-                cmap = discretized_cmaps[i].cpu()
-
-                # l = F.smooth_l1_loss(pred, label.permute(1, 2, 0), reduction="none", beta=0.01)
-                # print('diff in visualize', i, l.mean())
-                
-                result = T.concatenate([input_img, label], axis=2)
-                result = T.permute(result, (1, 2, 0))
-                result = self.unnormalize(result)
-                
-                result = T.concatenate([result, pred, cmap / 255.0], axis=1)
-                all_res.append(result)
-            all_res = T.concatenate(all_res, dim=0)
-            self.summary_writer.add_image(title, all_res, counter, dataformats='HWC')
-    
-    def iou(self, pred: T.tensor, label: T.tensor, mask: T.tensor, ori_label: T.tensor, color_palette: T.tensor):
-        pred = self.model.module.seggpt.unpatchify(pred) # B, 3, H, W
-        mask = mask[:, :, None].repeat(1, 1, self.model.module.seggpt.patch_size**2 * 3)
-        mask = self.model.module.seggpt.unpatchify(mask)
-        mask = mask.to(pred.device)
-        # print('mask', mask.shape)
-        # print('pred', pred.shape)
-        ori_label = ori_label.to(pred.device)
-        # print('ori_label', ori_label.shape)
-        color_palette = color_palette.to(pred.device)
-        # print('color_palette', color_palette.shape)
-
-        pred = T.permute(pred, (0, 2, 3, 1))  # B, H, W, 3
-        pred = self.unnormalize(pred)
-        discretized_cmap, pred_label = cmap_to_lbl(pred * 255.0, color_palette)
-        pred = pred[:, pred.shape[1]//2:, :, :]
-        pred_label = pred_label[:, pred_label.shape[1]//2:, :]
-        discretized_cmap = discretized_cmap[:, discretized_cmap.shape[1]//2:, :, :]
-        # print('pred_label', pred_label.shape, T.unique(pred_label))
-        # print('discretized_cmap', discretized_cmap.shape)
-        mask = mask[:, 0, :, :]
-        mask = mask[:, mask.shape[1]//2:, :] 
-        # print(mask, mask.shape)
-        n_class = color_palette.shape[1]
-        result = calculate_iou(pred_label, ori_label, mask, n_class)
-        return result, pred, discretized_cmap, mask
-    
-    def save_checkpoint(self, epoch: int, name: str = '', only_model: bool = True):
-        if self.gpu_id == 0:
-            save_checkpoint = {'model_state_dict': self.model.module.state_dict()}
-            if not only_model:
-                save_checkpoint['optimizer_state_dict'] = self.optim.state_dict()
-                save_checkpoint['scheduler_state_dict'] = self.scheduler.state_dict()
-            if name != '':
-                ckpt_path = os.path.join(self.args['ckpt_dir'], f'{name}.pt')
-            else:
-                ckpt_path = os.path.join(
-                    self.args['ckpt_dir'],
-                    f'epoch{epoch:02}_loss{self.last_loss:.4f}_metric{self.last_metric_val:.4f}.pt',
-                )
-            T.save(save_checkpoint, ckpt_path)
-            self.logger.info(f'Checkpoint saved to {ckpt_path}')
-
-    def process_data(self, dl: T.utils.data.DataLoader, is_train: bool, epoch: int):
-        if is_train:
-            self.logger.info('Training Phase')
-        elif not self.is_eval:
-            self.logger.info('Validation Phase')
-
-        n_class = dl.dataset.max_classes
-        iou = T.zeros((n_class, 2), device=self.gpu_id)
-        batch_losses = T.zeros(2, device=self.gpu_id)
-
-        pbar = tqdm(dl, disable=self.gpu_id != 0)
-
-        for i, (img, label, mask, valid, seg_type, ori_label, color_palette) in enumerate(pbar):
-            if not is_train:
-                self.model.eval()
-                with T.no_grad():
-                    b_loss, b_pred, b_mask = self.step(img, label, mask, valid, seg_type, is_train)
-                
-                c_iou, preds, cmaps, masks = self.iou(b_pred, label, mask, ori_label, color_palette)
-                val_counter = epoch * len(dl) + i
-                if val_counter % self.args['image_log_interval'] == 0:
-                    self.visualize(f'Validation', img, label, preds, cmaps, masks, val_counter)
-            else:
-                self.model.train()
-                b_loss, b_pred, b_mask = self.step(img, label, mask, valid, seg_type, is_train)
-                self.counter += 1
-
-                self.scheduler.step()
-                
-                c_iou, preds, cmaps, masks = self.iou(b_pred, label, mask, ori_label, color_palette)
-
-                self.write_summary(f'LR Scheduler', self.optim.param_groups[0]['lr'], self.counter)
-                self.write_summary('Training/Batch Loss', b_loss, self.counter)
-
-                self.visualize(f'Training', img, label, preds, cmaps, masks)
-                yield i
-
-
-            if self.gpu_id != 0:  # reset for gpu rank > 0
-                batch_losses = T.zeros(2, device=self.gpu_id)
-                iou = T.zeros((n_class, 2), device=self.gpu_id)
-
-            batch_losses[0] += b_loss
-            batch_losses[1] += 1
-            iou = iou + c_iou
-            
-            T.distributed.reduce(batch_losses, dst=0)
-            T.distributed.reduce(iou, dst=0)
-
-            avg_losses = batch_losses[0] / batch_losses[1]
-            m_iou = 100 * iou[:, 0] / (iou[:, 1] + 1e-10)
-            
-            # if is_train: # when training, only check iou for the first 7 classes (base)
-            #     m_iou = m_iou[:7].mean().item()
-            # else: # when validation, only check iou for the last 4 classes (novel)
-            #     m_iou = m_iou[:-4].mean().item()
-
-            pbar.set_postfix({
-                'Loss': f'{avg_losses:.5f}',
-                'mIoU': f'{m_iou[1:].mean().item():.3f}', # exclude background on mIoU
-                'IoU': ['%.3f' % x for x in m_iou.tolist()]
-            })
-
-        if not is_train:
-            self.last_loss = avg_losses
-            self.last_metric_val = m_iou[1:].mean().item()
-
-            self.write_summary('Validation/Loss', avg_losses, epoch)
-            self.write_summary('Validation/mIoU', m_iou[1:].mean().item(), epoch)
-        else:
-            self.write_summary('Training/Loss', avg_losses, epoch)
-            self.write_summary('Training/mIoU', m_iou[1:].mean().item(), epoch)
-
-        yield -1
-
-
-
 class AgentOneClass(Agent):
     def visualize(self, title:str, imgs: T.Tensor, labels: T.Tensor, preds: T.Tensor, discretized_cmaps: T.Tensor, masks: T.Tensor, class_label:T.tensor, counter:int = None):
         if self.log_enabled and self.gpu_id == 0 and (self.counter % self.args['image_log_interval'] == 0 or counter is not None):
@@ -608,3 +427,185 @@ class AgentOneClass(Agent):
             self.write_summary('Training/mIoU', weighted_m_iou, epoch)
 
         yield -1
+
+class AgentAdapter(Agent):
+    def step(
+        self,
+        img: T.Tensor,
+        label: T.Tensor,
+        mask: T.Tensor,
+        valid: T.Tensor,
+        seg_type: T.Tensor,
+        is_train: bool,
+    ):
+        img = img.to(self.gpu_id)
+        label = label.to(self.gpu_id)
+        mask = mask.to(self.gpu_id)
+        valid = valid.to(self.gpu_id)
+        seg_type = seg_type.to(self.gpu_id)
+
+        with T.cuda.amp.autocast():
+            feature_ensemble = -1 # TODO: test change to 0 to enable
+            loss, pred, bool_masked_pos = self.model(img, label, mask, valid, seg_type, feature_ensemble)
+
+        if is_train:
+            self.optim.zero_grad()
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optim)
+            self.scaler.update()
+        # print(pred.shape, pred[0].shape)
+        # print(tgts.shape, tgts[0].shape)
+        # a = self.model.module.seggpt.unpatchify(pred)
+        # a = a[0].permute(1,2,0)
+        # b = tgts[0].permute(1,2,0)
+        # print(a.shape, b.shape)
+        # a = a[a.shape[0]//2:, :, :]
+        # b = b[b.shape[0]//2:, :, :]
+        # print(a.shape, b.shape)
+        # c = T.cat([a, b], dim=1)
+        # print('loss before unnormalized', F.smooth_l1_loss(a, b, beta=0.01))
+        # self.summary_writer.add_image('testing view', c, 0, dataformats='HWC')
+        return loss.item(), pred, bool_masked_pos
+    
+    def visualize(self, title:str, imgs: T.Tensor, labels: T.Tensor, preds: T.Tensor, discretized_cmaps: T.Tensor, masks: T.Tensor, counter:int = None):
+        if self.log_enabled and self.gpu_id == 0 and (self.counter % self.args['image_log_interval'] == 0 or counter is not None):
+            counter = counter if counter is not None else self.counter
+            all_res = []
+            for i in range(len(preds)):
+                input_img = imgs[i].cpu()
+                label = labels[i].cpu()
+                pred = preds[i].cpu()
+                cmap = discretized_cmaps[i].cpu()
+
+                # l = F.smooth_l1_loss(pred, label.permute(1, 2, 0), reduction="none", beta=0.01)
+                # print('diff in visualize', i, l.mean())
+                
+                result = T.concatenate([input_img, label], axis=2)
+                result = T.permute(result, (1, 2, 0))
+                result = self.unnormalize(result)
+                
+                result = T.concatenate([result, pred, cmap / 255.0], axis=1)
+                all_res.append(result)
+            all_res = T.concatenate(all_res, dim=0)
+            self.summary_writer.add_image(title, all_res, counter, dataformats='HWC')
+    
+    def iou(self, pred: T.tensor, label: T.tensor, mask: T.tensor, ori_label: T.tensor, color_palette: T.tensor):
+        pred = self.model.module.seggpt.unpatchify(pred) # B, 3, H, W
+        mask = mask[:, :, None].repeat(1, 1, self.model.module.seggpt.patch_size**2 * 3)
+        mask = self.model.module.seggpt.unpatchify(mask)
+        mask = mask.to(pred.device)
+        # print('mask', mask.shape)
+        # print('pred', pred.shape)
+        ori_label = ori_label.to(pred.device)
+        # print('ori_label', ori_label.shape)
+        color_palette = color_palette.to(pred.device)
+        # print('color_palette', color_palette.shape)
+
+        pred = T.permute(pred, (0, 2, 3, 1))  # B, H, W, 3
+        pred = self.unnormalize(pred)
+        discretized_cmap, pred_label = cmap_to_lbl(pred * 255.0, color_palette)
+        pred = pred[:, pred.shape[1]//2:, :, :]
+        pred_label = pred_label[:, pred_label.shape[1]//2:, :]
+        discretized_cmap = discretized_cmap[:, discretized_cmap.shape[1]//2:, :, :]
+        # print('pred_label', pred_label.shape, T.unique(pred_label))
+        # print('discretized_cmap', discretized_cmap.shape)
+        mask = mask[:, 0, :, :]
+        mask = mask[:, mask.shape[1]//2:, :] 
+        # print(mask, mask.shape)
+        n_class = color_palette.shape[1]
+        result = calculate_iou(pred_label, ori_label, mask, n_class)
+        return result, pred, discretized_cmap, mask
+    
+    def save_checkpoint(self, epoch: int, name: str = '', only_model: bool = True):
+        if self.gpu_id == 0:
+            save_checkpoint = {'model_state_dict': self.model.module.state_dict()}
+            if not only_model:
+                save_checkpoint['optimizer_state_dict'] = self.optim.state_dict()
+                save_checkpoint['scheduler_state_dict'] = self.scheduler.state_dict()
+            if name != '':
+                ckpt_path = os.path.join(self.args['ckpt_dir'], f'{name}.pt')
+            else:
+                ckpt_path = os.path.join(
+                    self.args['ckpt_dir'],
+                    f'epoch{epoch:02}_loss{self.last_loss:.4f}_metric{self.last_metric_val:.4f}.pt',
+                )
+            T.save(save_checkpoint, ckpt_path)
+            self.logger.info(f'Checkpoint saved to {ckpt_path}')
+
+    def process_data(self, dl: T.utils.data.DataLoader, is_train: bool, epoch: int):
+        if is_train:
+            self.logger.info('Training Phase')
+        elif not self.is_eval:
+            self.logger.info('Validation Phase')
+
+        n_class = dl.dataset.max_classes
+        iou = T.zeros((n_class, 2), device=self.gpu_id)
+        batch_losses = T.zeros(2, device=self.gpu_id)
+
+        pbar = tqdm(dl, disable=self.gpu_id != 0)
+
+        for i, (img, label, mask, valid, seg_type, ori_label, color_palette) in enumerate(pbar):
+            if not is_train:
+                self.model.eval()
+                with T.no_grad():
+                    b_loss, b_pred, b_mask = self.step(img, label, mask, valid, seg_type, is_train)
+                
+                c_iou, preds, cmaps, masks = self.iou(b_pred, label, mask, ori_label, color_palette)
+                val_counter = epoch * len(dl) + i
+                if val_counter % self.args['image_log_interval'] == 0:
+                    self.visualize(f'Validation/{epoch}', img, label, preds, cmaps, masks, val_counter)
+            else:
+                self.model.train()
+                b_loss, b_pred, b_mask = self.step(img, label, mask, valid, seg_type, is_train)
+                self.counter += 1
+
+                self.scheduler.step()
+                
+                c_iou, preds, cmaps, masks = self.iou(b_pred, label, mask, ori_label, color_palette)
+
+                self.write_summary(f'LR Scheduler', self.optim.param_groups[0]['lr'], self.counter)
+                self.write_summary('Training/Batch Loss', b_loss, self.counter)
+
+                self.visualize(f'Training/{epoch}', img, label, preds, cmaps, masks)
+                yield i
+
+
+            if self.gpu_id != 0:  # reset for gpu rank > 0
+                batch_losses = T.zeros(2, device=self.gpu_id)
+                iou = T.zeros((n_class, 2), device=self.gpu_id)
+
+            batch_losses[0] += b_loss
+            batch_losses[1] += 1
+            iou = iou + c_iou
+            
+            T.distributed.reduce(batch_losses, dst=0)
+            T.distributed.reduce(iou, dst=0)
+
+            avg_losses = batch_losses[0] / batch_losses[1]
+            m_iou = 100 * iou[:, 0] / (iou[:, 1] + 1e-10)
+            
+            # if is_train: # when training, only check iou for the first 7 classes (base)
+            #     m_iou = m_iou[:7].mean().item()
+            # else: # when validation, only check iou for the last 4 classes (novel)
+            #     m_iou = m_iou[:-4].mean().item()
+
+            pbar.set_postfix({
+                'Loss': f'{avg_losses:.5f}',
+                'mIoU': f'{m_iou[1:].mean().item():.3f}', # exclude background on mIoU
+                'IoU': ['%.3f' % x for x in m_iou.tolist()]
+            })
+
+        if not is_train:
+            self.last_loss = avg_losses
+            self.last_metric_val = m_iou[1:].mean().item()
+
+            self.write_summary('Validation/Loss', avg_losses, epoch)
+            self.write_summary('Validation/mIoU', m_iou[1:].mean().item(), epoch)
+        else:
+            self.write_summary('Training/Loss', avg_losses, epoch)
+            self.write_summary('Training/mIoU', m_iou[1:].mean().item(), epoch)
+
+        yield -1
+
+
+

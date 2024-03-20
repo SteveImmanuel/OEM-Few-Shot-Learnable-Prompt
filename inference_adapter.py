8c00b4beeec69075b0516da79e7bbc8c1d1d9574
SteveImmanuel <steve@telepix.net>
2024-03-20 11:12:05 +0900

    Implement stitching for inference adapter

--- inference_adapter.py
+++ inference_adapter.py
@@ -42,7 +42,7 @@ def to_img_tensor(arr: np.ndarray, image_std, image_mean):
 
 
 @torch.no_grad()
-def run_one_image(img, tgt, model, device):
+def run_one_image(img, tgt, model, device, mask=None):
     x = torch.tensor(img)
     # make it a batch-like
     x = torch.einsum('nhwc->nchw', x)
@@ -51,9 +51,12 @@ def run_one_image(img, tgt, model, device):
     # make it a batch-like
     tgt = torch.einsum('nhwc->nchw', tgt)
 
-    bool_masked_pos = torch.zeros(model.seggpt.patch_embed.num_patches)
-    bool_masked_pos[model.seggpt.patch_embed.num_patches//2:] = 1
-    bool_masked_pos = bool_masked_pos.unsqueeze(dim=0)
+    if mask is None:
+        bool_masked_pos = torch.zeros(model.seggpt.patch_embed.num_patches)
+        bool_masked_pos[model.seggpt.patch_embed.num_patches//2:] = 1
+        bool_masked_pos = bool_masked_pos.unsqueeze(dim=0)
+    else:
+        bool_masked_pos = torch.tensor(mask).unsqueeze(dim=0)
     valid = torch.ones(tgt.shape[0], tgt.shape[1], tgt.shape[2] * 2, tgt.shape[3])
     seg_type = torch.zeros([valid.shape[0], 1])
     
@@ -64,7 +67,26 @@ def run_one_image(img, tgt, model, device):
 
     output = y[0, y.shape[1]//2:, :, :]
     output = torch.clip((output * imagenet_std + imagenet_mean) * 255, 0, 255)
-    return output
+    
+    mask = mask[:, :, None].repeat(1, 1, model.seggpt.patch_size**2 * 3)
+    mask = model.seggpt.unpatchify(mask)
+    # print('m', mask.shape)
+    mask = mask.permute(0, 2, 3, 1)
+    mask = mask[0, mask.shape[1]//2:, :, :]
+    mask = mask.cpu().float()
+    
+    # temp_img = x[0].permute(1, 2, 0)
+    # # print(temp_img.shape)
+    # # masked_label = (1 - mask) * temp_img  + mask * torch.randn_like(mask)
+    # # masked_label = torch.clip((masked_label * imagenet_std + imagenet_mean) * 255, 0, 255)
+    # masked_label = torch.clip((temp_img * imagenet_std + imagenet_mean) * 255, 0, 255)
+    # masked_label = masked_label.cpu().numpy()
+    # print(masked_label.shape)
+    # timg = Image.fromarray((masked_label).astype(np.uint8))
+    # timg.save('temp_test.png')
+
+
+    return output, mask
 
 def inference_image_with_crop(model, device, img_path, class_idx, outdir, split=2):
     res, hres = 448, 448
@@ -89,7 +111,7 @@ def inference_image_with_crop(model, device, img_path, class_idx, outdir, split=
             tgt = np.zeros_like(image)
 
             torch.manual_seed(2)
-            output = run_one_image(image, tgt, model, device)
+            output, _ = run_one_image(image, tgt, model, device)
             output = F.interpolate(
                 output[None, ...].permute(0, 3, 1, 2), 
                 size=[row_size, col_size], 
@@ -118,67 +140,116 @@ def inference_image_with_crop(model, device, img_path, class_idx, outdir, split=
     concat.save(os.path.join(outdir, 'concat', filename.replace('.tif', '.png')))
     final_out_label.save(os.path.join(outdir, 'label', filename.replace('.tif', '.png')))
 
-setup_logging()
-logger = get_logger(__name__, 0)
-model_path = 'logs/1710831235/weights/best.pt'
+
+def create_stitch_mask(h, w, type, width):
+    prompt_mask = np.zeros(h * w)
+    image_mask = np.zeros((h, w))
+    if type == 0:
+        image_mask[:, image_mask.shape[1] // 2 - width: image_mask.shape[1] // 2 + width] = 1
+    elif type == 1:
+        image_mask[image_mask.shape[0] // 2 - width: image_mask.shape[0] // 2 + width, :] = 1
+    else:
+        image_mask[image_mask.shape[0] // 2 - width: image_mask.shape[0] // 2 + width, image_mask.shape[1] // 2 - width: image_mask.shape[1] // 2 + width] = 1
+    image_mask = image_mask.flatten()
+    result = np.concatenate((prompt_mask, image_mask))
+    return result
+
+def inference_stitch(model, device, img_path, tgt_path, lbl_path, out_dir, split=2, width=4):
+    # run after inference_image_with_crop
+    # only works for split = 2
+    res, hres = 448, 448
+
+    full_image = Image.open(img_path).convert('RGB').resize((1024, 1024))
+    full_tgt = Image.open(tgt_path).convert('RGB').resize((1024, 1024), Image.NEAREST)
+    full_lbl = Image.open(lbl_path).convert('L').resize((1024, 1024), Image.NEAREST)
+    col_size = full_image.size[0] // split
+    row_size = full_image.size[1] // split
+    
+    w, h = full_image.size
+    final_out_color = np.array(full_tgt)
+    final_out_label = np.array(full_lbl)
+
+    crop_params = [
+        [(w // 4, 0, 3 * w // 4, h // 2), 0], # top middle
+        [(w // 4, h // 2, 3 * w // 4, h), 0], # bottom middle
+        [(0, h // 4, w // 2, 3 * h // 4), 1], # left middle
+        [(w // 2, h // 4, w, 3 * h // 4), 1], # right middle
+        [(w // 4, h // 4, 3 * w // 4, 3 * h // 4), 2] # center
+    ]
+
+    for i, (crop_param, stitch_type) in enumerate(crop_params):
+        j1, i1, j2, i2 = crop_param
+        assert j2 - j1 == col_size and i2 - i1 == row_size
+
+        cropped_image = full_image.crop(crop_param).resize((res, hres))
+        cropped_tgt = full_tgt.crop(crop_param).resize((res, hres), Image.NEAREST)
+
+        # cropped_image.save(f'temp_cropped_image{i}.png')
+        # cropped_tgt.save(f'temp_cropped_tgt{i}.png')
+
+        img = np.array(cropped_image) / 255.
+        img = (img - imagenet_mean) / imagenet_std
+        img = np.expand_dims(img, axis=0)
+        tgt = np.array(cropped_tgt) / 255.
+        tgt = (tgt - imagenet_mean) / imagenet_std
+        tgt = np.expand_dims(tgt, axis=0)
+
+        torch.manual_seed(2)
+        hstitch_mask = create_stitch_mask(28, 28, stitch_type, width)
+        output, mask = run_one_image(img, tgt, model, device, hstitch_mask)
+        output = F.interpolate(
+            output[None, ...].permute(0, 3, 1, 2), 
+            size=[row_size, col_size], 
+            mode='nearest',
+        ).permute(0, 2, 3, 1)
+        mask = F.interpolate(
+            mask[None, ...].permute(0, 3, 1, 2), 
+            size=[row_size, col_size], 
+            mode='nearest',
+        ).permute(0, 2, 3, 1)
+        output, label = cmap_to_lbl(output, torch.tensor(color_map, device=output.device, dtype=output.dtype).unsqueeze(0))
+        output = output[0].numpy()
+        label = label[0].numpy()
+        mask = mask[0].numpy()
+
+        final_out_color[i1:i2, j1:j2] = output * mask + final_out_color[i1:i2, j1:j2] * (1 - mask)
+        final_out_label[i1:i2, j1:j2] = label * mask[:, :, 0] * class_idx + final_out_label[i1:i2, j1:j2] * (1 - mask[:, :, 0])
+
+    
+    filename = os.path.basename(img_path).replace('.tif', '.png')
+    os.makedirs(os.path.join(out_dir, 'color'), exist_ok=True)
+    os.makedirs(os.path.join(out_dir, 'concat'), exist_ok=True)
+    os.makedirs(os.path.join(out_dir, 'label'), exist_ok=True)
+
+    final_out_color = Image.fromarray((final_out_color).astype(np.uint8))
+    final_out_color.save(os.path.join(out_dir, 'color', filename))
+
+    final_out_label = Image.fromarray((final_out_label).astype(np.uint8))
+    final_out_label.save(os.path.join(out_dir, 'label', filename))
+
+    concat = np.concatenate((np.array(full_image), np.array(full_tgt), final_out_color), axis=1)
+    concat = Image.fromarray((concat).astype(np.uint8))
+    concat.save(os.path.join(out_dir, 'concat', filename))
+
+model_path = 'logs/1710835935/weights/best.pt'
 seggpt_model = seggpt_vit_large_patch16_input896x448()
-initial_ckpt = T.load(model_path, map_location='cpu')
-seggpt_model.load_state_dict(initial_ckpt['model_state_dict'], strict=False)
 model = AdapterSegGPT(seggpt_model)
-logger.info('Frozen model loaded')
+print('Frozen model loaded')
 
 ckpt = T.load(model_path, map_location='cpu')
 model.load_state_dict(ckpt['model_state_dict'])
-logger.info('Checkpoint loaded')
+print('Checkpoint loaded')
 
 model = model.to(0)
 model.eval()
 
 dataset_dir = '/disk3/steve/dataset/OpenEarthMap-FSS/testset/images'
-outdir = 'class_novel/8_phase2'
-split = 4
+outdir = 'class_novel/11_phase2'
+class_idx = 11
+split = 2
 for file in tqdm(os.listdir(dataset_dir)):
-    inference_image_with_crop(model, 'cuda', os.path.join(dataset_dir, file), class_idx=9, outdir=outdir, split=split)
-    # full_image = Image.open(os.path.join(dataset_dir, file)).resize((896, 896))
-    # row_size = full_image.size[0] // split
-    # col_size = full_image.size[1] // split
-
-    # h, w = full_image.size
-    # final_out_color = np.zeros((h, w, 3))
-    # # final_out_label = np.zeros((h, w))
-    # final_out_image = np.zeros((h, w, 3))
-
-    # for row in range(split):
-    #     for col in range(split):
-    #         c_img = full_image.crop((row * row_size, col * col_size, (row + 1) * row_size, (col + 1) * col_size))
-    #         c_img = np.array(c_img.resize((448, 448))).astype(np.uint8)
-    #         img = to_img_tensor(c_img, train_args['image_std'], train_args['image_mean'])
-
-    #         img = img.unsqueeze(0).to(0)
-
-    #         zero_label = T.zeros_like(img)
-    #         with T.no_grad():
-    #             _, pred, _ = model(img, zero_label, mask, valid, seg_type, -1)
-            
-    #         pred = model.seggpt.unpatchify(pred)
-    #         print(pred.shape)
-    #         img = img[0]
-    #         pred = pred[0]
-    #         pred = pred[:, pred.shape[1]//2:, :]
-    #         print(img.shape, pred.shape)
-    #         print(row, col)
-
-    #         pred = pred.permute(1, 2, 0)
-    #         pred = pred.cpu().numpy()
-            
-    #         final_out_color[col * col_size:(col + 1) * col_size, row * row_size:(row + 1) * row_size] = pred
-    #         # final_out_label[col * col_size:(col + 1) * col_size, row * row_size:(row + 1) * row_size] = label
-    #         final_out_image[col * col_size:(col + 1) * col_size, row * row_size:(row + 1) * row_size] = c_img
-
-    # res = np.concatenate((final_out_image, final_out_color), axis=1)
-    # res = (res * train_args['image_std'] + train_args['image_mean']) * 255
-    # # res = unnormalize(res, train_args['image_std'], train_args['image_mean'])
-    # # res_numpy = res.cpu().numpy()
-    # img = Image.fromarray(res.astype(np.uint8))
-    # img.save(f'restemp/res_{file}.png')
-
+    inference_image_with_crop(model, 'cuda', os.path.join(dataset_dir, file), class_idx=class_idx, outdir=outdir, split=split)
+    tgt_path = os.path.join(outdir, 'color', file.replace('.tif', '.png'))
+    lbl_path = os.path.join(outdir, 'label', file.replace('.tif', '.png'))
+    outdir_stitch = f'{outdir}_stitch'
+    inference_stitch(model, 'cuda', os.path.join(dataset_dir, file), tgt_path, lbl_path, outdir_stitch, split=split, width=4)
